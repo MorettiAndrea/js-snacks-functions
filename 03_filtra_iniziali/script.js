8ea1b51f8cedb98b9569869e0e8a79ff3fce21e4
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-04-08 22:29:49 +0200

    fine terzo esercizio

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,21 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+function filterByLetter(list) {
+  const filtredArray = [];
+
+  for (let i = 0; i < list.length; i++) {
+    const currentName = list[i];
+    if (currentName[0] === "A") {
+      filtredArray.push(currentName);
+    }
+  }
+  return filtredArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(filterByLetter(names));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
