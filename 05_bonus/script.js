086b88262f5d4d08c4c99d8e1ca59370869d0aae
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-04-08 23:10:15 +0200

    fine bonus da controllare

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,25 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
+function sayHi(person) {
+  const hour = new Date().getHours();
+  let targetName = person;
+  let hello;
 
+  if (hour < 13) {
+    hello = "Buongiorno" + " " + targetName;
+  } else if (hour < 17) {
+    hello = "Buon pomeriggio" + " " + targetName;
+  } else {
+    hello = "Buonasera" + " " + targetName;
+  }
+  return hello;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(sayHi(name));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
