631999ce6f7c542b85b0c6b47185bbc18c31f435
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-04-08 22:42:36 +0200

    fine 4o

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,26 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
-
+function vowelsCounter(targetWord) {
+  let vowelsTotal = 0;
+  for (let i = 0; i < targetWord.length; i++) {
+    const vowelsChecker = targetWord[i].toLowerCase();
+    if (
+      vowelsChecker === "a" ||
+      vowelsChecker === "e" ||
+      vowelsChecker === "i" ||
+      vowelsChecker === "o" ||
+      vowelsChecker === "u"
+    ) {
+      vowelsTotal += 1;
+    }
+  }
+  return vowelsTotal;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(vowelsCounter(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
